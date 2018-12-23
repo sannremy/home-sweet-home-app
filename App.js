f9d8e49c45e7ae83478a06fa40b305f74390e630
Sann-Remy Chea <sannremy.chea@gmail.com>
2018-12-23 15:37:11 +0100

    Added layout

--- App.js
+++ App.js
@@ -1,20 +1,71 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { Alert, StyleSheet, Text, View, ScrollView, SafeAreaView, TouchableOpacity } from 'react-native';
 
 export default class App extends React.Component {
+  _onPressButton() {
+    Alert.alert('You tapped the button!')
+  }
+
   render() {
     return (
-      <View style={styles.container}>
-        <Text>Test</Text>
-      </View>
+      <SafeAreaView style={{flex: 1}}>
+        {/* Header menu */}
+        <View style={styles.header}>
+          <Text style={styles.headerText}>Title</Text>
+        </View>
+
+        {/* Content */}
+        <ScrollView>
+          <Text style={{fontSize:96}}>Scroll me plz</Text>
+        </ScrollView>
+
+        {/* Footer menu */}
+        <View style={{flex: 0.15, flexDirection: 'row', backgroundColor: 'powderblue'}}>
+          <TouchableOpacity onPress={this._onPressButton} style={styles.menuItem}>
+            <View>
+              <Text>Menu 1</Text>
+            </View>
+          </TouchableOpacity>
+          <TouchableOpacity onPress={this._onPressButton} style={styles.menuItem}>
+            <View>
+              <Text>Menu 1</Text>
+            </View>
+          </TouchableOpacity>
+          <TouchableOpacity onPress={this._onPressButton} style={styles.menuItem}>
+            <View>
+              <Text>Menu 1</Text>
+            </View>
+          </TouchableOpacity>
+          <TouchableOpacity onPress={this._onPressButton} style={styles.menuItem}>
+            <View>
+              <Text>Menu 1</Text>
+            </View>
+          </TouchableOpacity>
+          <TouchableOpacity onPress={this._onPressButton} style={styles.menuItem}>
+            <View>
+              <Text>Menu 1</Text>
+            </View>
+          </TouchableOpacity>
+        </View>
+      </SafeAreaView>
     );
   }
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
+  header: {
+    flex: 0.15,
+    backgroundColor: 'powderblue',
+    alignItems: 'center',
+    justifyContent: 'center',
+    fontSize: 44
+  },
+  headerText: {
+    fontSize: 44
+  },
+  menuItem: {
+    flex: 0.2,
+    backgroundColor: 'powderblue',
     alignItems: 'center',
     justifyContent: 'center',
   },
