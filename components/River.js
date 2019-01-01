39b78a5a98a94819d389d489417a7f733fec16c6
Sann-Remy Chea <sannremy.chea@gmail.com>
2019-01-01 12:51:47 +0100

    Added icon for river

--- components/River.js
+++ components/River.js
@@ -6,6 +6,8 @@ import {
   View
 } from 'react-native';
 
+import Icon from 'react-native-vector-icons/Feather';
+
 export default class River extends React.Component {
 
   constructor(props) {
@@ -13,8 +15,28 @@ export default class River extends React.Component {
   }
 
   render() {
+    // Icon
+    const info = {
+      name: 'help-circle', // Unknown
+      size: 20,
+      color: this.props.data.color
+    };
+
+    if (this.props.data.color === 'green') {
+      info.name = 'check-circle';
+    } else if (this.props.data.color === 'red') {
+      info.name = 'alert-circle';
+    } else if (this.props.data.color === 'orange') {
+      info.name = 'alert-circle';
+    } else if (this.props.data.color === 'yellow') {
+      info.name = 'alert-circle';
+    }
+
+    // Date
+
     return (
       <View style={styles.box}>
+        <Icon name={info.name} size={info.size} color={info.color} />
         <Text>{this.props.data.date}</Text>
         <Text>{this.props.data.level}</Text>
         <Text>{this.props.data.color}</Text>
