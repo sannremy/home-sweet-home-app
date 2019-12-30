3d4ff530a151310bc5d394e6b15835cf3236b21c
Sann-Remy Chea <sannremy.chea@gmail.com>
2019-12-30 21:04:02 +0100

    iOS 9.3 - old iPad compatibility

--- components/Control.js
+++ components/Control.js
@@ -18,6 +18,25 @@ import { Col, Row, Grid } from 'react-native-easy-grid';
 import Icon from 'react-native-vector-icons/Feather';
 import moment from 'moment/min/moment-with-locales';
 
+// https://github.com/uxitten/polyfill/blob/master/string.polyfill.js
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/padStart
+// iOS 9.3
+if (!String.prototype.padStart) {
+  String.prototype.padStart = function padStart(targetLength, padString) {
+      targetLength = targetLength >> 0; //truncate if number, or convert non-number to 0;
+      padString = String(padString !== undefined ? padString : ' ');
+      if (this.length >= targetLength) {
+          return String(this);
+      } else {
+          targetLength = targetLength - this.length;
+          if (targetLength > padString.length) {
+              padString += padString.repeat(targetLength / padString.length); //append to original to ensure we are longer than needed
+          }
+          return padString.slice(0, targetLength) + String(this);
+      }
+  };
+}
+
 type Props = {
   data: Object,
   locale: String
